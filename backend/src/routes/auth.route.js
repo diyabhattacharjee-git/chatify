70f1af0b5f78d54c21c91bb892c2961eb50b7886
Diya Bhattacharjee <dipabhattacharjee720@gmail.com>
2026-03-14 14:58:06 +0530

    upto rate-limiting implemented

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,21 +1,21 @@
 import express from "express";
+import { signup, login, logout, updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
-router.get("/signup", (req,res) => {
-    res.send("Signup endpoint");
-})
-router.get("/login", (req,res) => {
-    res.send("Login endpoint");
-})
+router.use(arcjetProtection)
 
-router.get("/logout", (req,res) => {
-    res.send("Logout endpoint");
-})
 
-router.get("/api/auth/update", (req,res) => {
-    res.send("Update endpoint");
-})
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout)
+
+router.put("/update-profile", protectRoute,updateProfile);
+
+router.get("/check", protectRoute, (req, res) => res.status(200).json(req.user))
+
 
 
 export default router;
