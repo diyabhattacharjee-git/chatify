d7daa36b26fd9a24ef1b095b8d90296a18d5fcdd
Diya Bhattacharjee <dipabhattacharjee720@gmail.com>
2026-03-16 21:17:31 +0530

    frontend codebase setup

---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function SignUpPage() {
+  const {authUser, isLoading, login} = useAuthStore()
+  return (
+    <div>
+      SignUpPages
+    </div>
+  )
+}
+
+export default SignUpPage
