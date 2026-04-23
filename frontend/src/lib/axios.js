2d9cfd59ed39f39ac7a5d642f829a2221b6fed4b
Diya Bhattacharjee <dipabhattacharjee720@gmail.com>
2026-04-23 20:12:08 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://chat-backend-b8ja.onrender.com/api",
+  // baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://chat-backend-b8ja.onrender.com/api",
+  baseURL: "https://chat-backend-b8ja.onrender.com/api",
   withCredentials: true,
 });
