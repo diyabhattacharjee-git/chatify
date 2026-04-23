8ac37342d702fe865af2f0f81cbef21f0c4d73e1
Diya Bhattacharjee <dipabhattacharjee720@gmail.com>
2026-04-23 14:34:16 +0530

    online users implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,11 +7,12 @@ import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 import { connectDB } from "./lib/db.js"
 import {ENV} from "./lib/env.js"
+import { app, server } from "./lib/socket.js"
 import cors from "cors"
 
 // dotenv.config();
 
-const app = express();
+// const app = express();
 const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
@@ -41,7 +42,7 @@ if(process.env.NODE_ENV ==="production"){
 // });
 
 connectDB().then(() => {
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
         console.log(`Server running on port: ${PORT}`);
     });
 })
--- frontend/src/components/ContactList.jsx
+++ frontend/src/components/ContactList.jsx
@@ -1,11 +1,11 @@
 import { useEffect } from "react";
 import { useChatStore } from "../store/useChatStore";
 import UsersLoadingSkeleton from "./UsersLoadingSkeleton";
-// import { useAuthStore } from "../store/useAuthStore";
+import { useAuthStore } from "../store/useAuthStore";
 
 function ContactList() {
-  const { getAllContacts, allContacts, setSelecteduser, isUsersLoading } = useChatStore();
-  // const { onlineUsers } = useAuthStore();
+  const { getAllContacts, allContacts, setSelectedUser, isUsersLoading } = useChatStore();
+  const { onlineUsers } = useAuthStore();
 
   useEffect(() => {
     getAllContacts();
@@ -19,11 +19,10 @@ function ContactList() {
         <div
           key={contact._id}
           className="bg-cyan-500/10 p-4 rounded-lg cursor-pointer hover:bg-cyan-500/20 transition-colors"
-          onClick={() => setSelecteduser(contact)}
+          onClick={() => setSelectedUser(contact)}
         >
           <div className="flex items-center gap-3">
-            {/* make it work with socket*/}
-            <div className={`avatar online`}>
+            <div className={`avatar ${onlineUsers.includes(contact._id) ? "online" : "offline"}`}>
               <div className="size-12 rounded-full">
                 <img src={contact.profilePic || "/avatar.png"} />
               </div>
